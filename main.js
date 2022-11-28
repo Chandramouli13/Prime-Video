e117b5fdeca41677e51565d6035a0376b4085d89
Chandramouli13 <chandurajesh13@gmail.com>
2022-11-28 15:39:04 +0530

    prime

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// Dark/Light mode
+const changeMode = () => {
+    let mybody = document.body;
+    mybody.classList.toggle('myLight');
+}
+
+// coupon code
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('main').style.opacity = '0.3';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1';
+}
+
+
+
+
